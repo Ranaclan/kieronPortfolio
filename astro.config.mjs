829900ed24ce23f5dcf3e56924266bfeaed1a946
Kieron <kieronrana@gmail.com>
2025-03-13 11:07:38 +0000

    github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  site: 'https://ranaclan.github.io',
+  base: '/kieronPortfolio',
   integrations: [react(), tailwind()]
 });
