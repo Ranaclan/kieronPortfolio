72b456cba0a305acbcef50f739dc45d6e75458ad
Kieron <kieronrana@gmail.com>
2025-03-13 11:31:32 +0000

    fixed root

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,11 +22,11 @@ const Header: React.FC = () => {
   return (
     <header className={`bg-gray-900 text-white p-4 transition-all duration-300 ${isSticky ? 'fixed top-0 left-0 right-0 shadow-md z-50' : ''}`}>
       <nav className="container mx-auto flex justify-between items-center">
-        <a href="/" className="text-xl font-bold">Kieron Rana</a>
+        <a href="/kieronPortfolio" className="text-xl font-bold">Kieron Rana</a>
         <ul className="flex space-x-4">
-          <li><a href="/#about" className="hover:text-gray-300 transition-colors">About</a></li>
-          <li><a href="/#skills" className="hover:text-gray-300 transition-colors">Skills</a></li>
-          <li><a href="/#projects" className="hover:text-gray-300 transition-colors">Projects</a></li>
+          <li><a href="/kieronPortfolio/#about" className="hover:text-gray-300 transition-colors">About</a></li>
+          <li><a href="/kieronPortfolio/#skills" className="hover:text-gray-300 transition-colors">Skills</a></li>
+          <li><a href="/kieronPortfolio/#projects" className="hover:text-gray-300 transition-colors">Projects</a></li>
           <li><a href="src\cv\Kieron Rana CV.docx.pdf" target="_blank" rel="noopener noreferrer" className="hover:text-gray-300 transition-colors">CV</a></li>
           <li><a href="https://www.linkedin.com/in/kieronrana/" target="_blank" rel="noopener noreferrer" className="hover:text-gray-300 transition-colors">LinkedIn</a></li>
           <li><a href="mailto:kieronrana@gmail.com" target="_blank"  className="hover:text-gray-300 transition-colors">Contact</a></li>
